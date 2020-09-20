3340d61b55b7568da23116e6ff4267b7c5a1429e
goyounha11 <goyounha11@naver.com>
2020-09-20 15:48:25 +0900

    Updates

---
+++ precache-manifest.7f986fa7738174890edd2445d52abeaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6199089501b55275951f9adfde342d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5cd3ae1886e8b279a7f6",
+    "url": "/movie_app_2020/static/css/main.3f07181a.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd3ae1886e8b279a7f6",
+    "url": "/movie_app_2020/static/js/main.d0ba3bc4.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
